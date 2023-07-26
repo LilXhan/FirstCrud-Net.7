b7db6e8fa3e3d76bc5619188fe6a04d7c115cdfa
Flavio Alvarado Tucto <alvaradotuctod@gmail.com>
2023-07-26 10:16:33 -0500

    Agregar archivos de proyecto.

---
+++ BlogCore/Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using BlogCore.Data;
+using BlogCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace BlogCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contact.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contact.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var contact = _context.Contact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contact.Update(contact);   
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        { 
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = _context.Contact.Find(id);
+
+            if (contact == null)
+
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = _context.Contact.Find(id);
+            
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteContact(int? id)
+        {
+            var contact = await _context.Contact.FindAsync(id);
+
+            if (contact == null)
+            {
+                return View();
+            }
+
+            _context.Contact.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BlogCore/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogCore.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public string Smarthphone { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
